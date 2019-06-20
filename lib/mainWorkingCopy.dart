45bac7878ad8c82e916b51195c8ce2bddbfdbc3b
Cicada501 <louis.donath@online.de>
2019-06-20 11:36:43 +0200

    Projekt komplett hinzugefügt

---
+++ lib/mainWorkingCopy.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+// ------------------- TO DO LIST -------------------------------------------------
+
+ List<String> _todoItems = [];
+
+  // This will be called each time the + button is pressed
+  void _addTodoItem() {
+    // Putting our code inside "setState" tells the app that our state has changed, and
+    // it will automatically re-render the list
+    setState(() {
+      int index = _todoItems.length;
+      _todoItems.add('Item ' + index.toString());
+    });
+  }
+
+  // Build the whole list of todo items
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        // itemBuilder will be automatically be called as many times as it takes for the
+        // list to fill up its available space, which is most likely more than the
+        // number of todo items we have. So, we need to check the index is OK.
+        if(index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index]);
+        }
+      },
+    );
+  }
+
+  // Build a single todo item
+  Widget _buildTodoItem(String todoText) {
+    return new ListTile(
+      title: new Text(todoText)
+    );
+}
+
+
+// -------------------------END: To DO LIST --------------------------------------
+
+
+  int _selectedIndex = 0;
+
+static const TextStyle mytextStyle = TextStyle( fontSize: 20,
+                                                fontWeight: FontWeight.bold, 
+                                                letterSpacing: 1.0,
+                                                fontFamily: "Sans",
+                                                decoration: TextDecoration.underline
+                                                 );
+static const List<Widget> _widgetOptions = <Widget>[
+  Text(
+    'Calendar',
+    style: mytextStyle
+  ),
+  Text(
+     'To Do List',
+     style: mytextStyle,
+  ),
+  
+];
+
+
+
+void _onItemTapped(int index) {
+  setState(() {
+    _selectedIndex = index;
+  });
+}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar( backgroundColor: Colors.greenAccent,
+      title: const Text('Kalendo',  textAlign: TextAlign.center,  ),
+    ),
+    body: Padding(
+      padding: EdgeInsets.all(18.0),
+      child: _widgetOptions.elementAt(_selectedIndex),
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today),
+          title: Text('Calendar'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.list),
+          title: Text('ToDo'),
+        ),
+       
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.amber[800],
+      onTap: _onItemTapped,
+    ),
+  );
+}
+}
